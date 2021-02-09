1dfbd20822514f9924cc7f9f20a0dedbd77e7288
Al-Hossam <Al-Hossam@DESKTOP-8UMV3MV>
2021-02-09 15:13:04 +0300

    Add  question number 3 in level 4

--- SoftWare Engineering/LevelQ4_3.cs
+++ SoftWare Engineering/LevelQ4_3.cs
@@ -16,5 +16,52 @@ namespace SoftWare_Engineering
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+            Function.Undo();
+            this.Hide();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BOK_Click(object sender, EventArgs e)
+        {
+            if (ANSWER1.Checked == true && ANSWER2.Checked == false && ANSWER3.Checked == false)
+            {
+                Function.counter++;
+
+                COUNTER.Text = Function.counter.ToString();
+
+            }
+            else if (ANSWER1.Checked == false && ANSWER2.Checked == true && ANSWER3.Checked == false)
+            {
+                Function.counter++;
+
+                COUNTER.Text = Function.counter.ToString();
+
+            }
+            else if (ANSWER1.Checked == false && ANSWER2.Checked == false && ANSWER3.Checked == true)
+            {
+                star();
+                MessageBox.Show(" The Level Four has been completed ");
+                Function.HowDigree(Function.counter);
+                Form1 f = new Form1();
+                f.Show();
+                this.Hide();
+            }
+        }
+             public void star()
+        {
+
+            STAR1.ImageIndex = 1;
+            STAR2.ImageIndex = 1;
+            STAR3.ImageIndex = 1;
+        }
+    
     }
 }
